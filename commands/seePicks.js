08fb1804b24ba1af2d27440441367e71fbff8a1c
tugboat0278 <tugboat0278@gmail.com>
2026-09-05 02:52:38 -0400

    Update seePicks.js

--- commands/seePicks.js
+++ commands/seePicks.js
@@ -3,66 +3,89 @@ const userSchema = require('../schemas/user-schema');
 const nfl = require('../NFL_Teams.js');
 
 module.exports = {
-    commands: ['seepicks', 'seePicks', 'SeePicks'],
-    expectedArgs: '<week>',
-    minArgs: 1,
-    maxArgs: 1,
-    callback: async (message, arguments, text, client, mongo, Discord) => {
-        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        const week = arguments[0];
-        const authorId = message.author.id;
+  commands: ['seepicks'],
+  alias: 'seePicks',
+  expectedArgs: '<week>',
+  minArgs: 1,
+  maxArgs: 1,
 
-        if (week < 1 || week > 22 || isNaN(Number(week))) {
-            return message.reply('Sorry, that week is invalid');
-        }
+  callback: async (message, arguments, text, client, mongo, Discord) => {
+    const week = Number(arguments[0]);
+    const authorId = message.author.id;
 
-        const createEmbed = function (scheduleData, picksData) {
-            const embed = new Discord.MessageEmbed();
-
-            embed.setTitle(`Your picks for week ${week}`).setColor(randomColor);
-            let count = 0;
-
-            for (let i = 0; i < scheduleData.length - 1; i += 2) {
-
-                const homeTeam = nfl.nfl[scheduleData[i]].name;
-                const awayTeam = nfl.nfl[scheduleData[i + 1]].name;
-
-                const userPicks = nfl.nfl[picksData[count]].name;
-
-                const embedString = userPicks == homeTeam ? `You picked the __**${homeTeam}**__ to beat the ${awayTeam}` : `You picked the __**${awayTeam}**__ to beat the ${homeTeam}`;
-
-                embed.addField(`Game ${count + 1}`, embedString);
-                count++;
-            }
-            message.channel.send(embed);
-        }
-
-        const fetchFromMongoDB = async () => {
-            await mongo().then(async (mongoose) => {
-                try {
-                    console.log('Connected to MongoDB');
-                    //Pull schedule data from DB
-                    const userObject = await userSchema.find({ id: `${authorId}` });
-                    const scheduleObject = await scheduleSchema.find({ week: `${week}` });
-
-                    if (!scheduleObject) {
-                        return message.reply(`Uh Oh. Something is wrong with the schedule for week ${week}`)
-                    }
-
-                    const picksArray = userObject[0].picks[week];
-                    if (!picksArray) {
-                        return message.reply(`It doesn't look like you have any picks saved for week ${week}`)
-                    }
-                    const scheduleArray = scheduleObject[0].games;
-
-                    createEmbed(scheduleArray, picksArray);
-
-                } finally {
-                    console.log('Closing MongoDB Connection');
-                    mongoose.connection.close();
-                }
-            })
-        }
-        fetchFromMongoDB();
+    if (Number.isNaN(week) || week < 1 || week > 22) {
+      return message.reply('Sorry, that week is invalid.');
     }
-}
+
+    try {
+      await mongo();
+
+      const user = await userSchema.findOne({
+        id: authorId
+      });
+
+      if (!user) {
+        return message.reply(
+          "It doesn't look like you have any saved picks yet."
+        );
+      }
+
+      const scheduleData = await scheduleSchema.findOne({
+        week: String(week)
+      });
+
+      if (!scheduleData) {
+        return message.reply(
+          `I couldn't find the NFL schedule for Week ${week}.`
+        );
+      }
+
+      const picksArray = user.picks?.[week];
+
+      if (!picksArray || picksArray.length === 0) {
+        return message.reply(
+          `It doesn't look like you have any picks saved for Week ${week}.`
+        );
+      }
+
+      const schedule = scheduleData.games;
+
+      const embed = new Discord.EmbedBuilder()
+        .setTitle(`🏈 Your Picks for Week ${week}`)
+        .setColor(Math.floor(Math.random() * 0xffffff));
+
+      let pickIndex = 0;
+
+      for (let i = 0; i < schedule.length - 1; i += 2) {
+        const awayCode = schedule[i];
+        const homeCode = schedule[i + 1];
+        const pickedCode = picksArray[pickIndex];
+
+        const awayName = nfl.nfl[awayCode]?.name || awayCode.toUpperCase();
+        const homeName = nfl.nfl[homeCode]?.name || homeCode.toUpperCase();
+        const pickedName = nfl.nfl[pickedCode]?.name || pickedCode?.toUpperCase();
+
+        const opponentName =
+          pickedCode === awayCode ? homeName : awayName;
+
+        embed.addFields({
+          name: `Game ${pickIndex + 1}`,
+          value: `You picked **${pickedName}** to beat **${opponentName}**`
+        });
+
+        pickIndex++;
+      }
+
+      return message.channel.send({
+        embeds: [embed]
+      });
+
+    } catch (error) {
+      console.error('seePicks command error:', error);
+
+      return message.reply(
+        'There was an error loading your picks.'
+      );
+    }
+  }
+};
