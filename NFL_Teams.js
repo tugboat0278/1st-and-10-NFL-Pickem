420aadecadce13d14ce96a450d6257cd6ae05729
tugboat0278 <tugboat0278@gmail.com>
2026-09-05 14:47:08 -0400

    Update NFL_Teams.js

--- NFL_Teams.js
+++ NFL_Teams.js
@@ -1,133 +1,132 @@
 const nfl = {
-    Ari: {
-        name: "Cardinals",
-        id: "620701250316533764"
-    },
-    Atl: {
-        name: "Falcons",
-        id: "620701260542377985"
-    },
-    Bal: {
-        name: "Ravens",
-        id: "620701260756156417"
-    },
-    Buf: {
-        name: "Bills",
-        id: "620701258814324738"
-    },
-    Car: {
-        name: "Panthers",
-        id: "620701255412613140"
-    },
-    Chi: {
-        name: "Bears",
-        id: "620701259489607693"
-    },
-    Cin: {
-        name: "Bengals",
-        id: "620701248886276097"
-    },
-    Cle: {
-        name: "Browns",
-        id: "620701261842350091"
-    },
-    Dal: {
-        name: "Cowboys",
-        id: "620701258067476510"
-    },
-    Den: {
-        name: "Broncos",
-        id: "620701259057594391"
-    },
-    Det: {
-        name: "Lions",
-        id: "620701260764545025"
-    },
-    Gb: {
-        name: "Packers",
-        id: "620701258982096956"
-    },
-    Hou: {
-        name: "Texans",
-        id: "620701259464179747"
-    },
-    Ind: {
-        name: "Colts",
-        id: "620701258075996200"
-    },
-    Jax: {
-        name: "Jaguars",
-        id: "620701261787955210"
-    },
-    Kc: {
-        name: "Chiefs",
-        id: "620701258272997406"
-    },
-    Lac: {
-        name: "Chargers",
-        id: "620701258516529162"
-    },
-    Lar: {
-        name: "Rams",
-        id: "741681763621011477"
-    },
-    Mia: {
-        name: "Dolphins",
-        id: "620701443405643776"
-    },
-    Min: {
-        name: "Vikings",
-        id: "620701261762920448"
-    },
-    Ne: {
-        name: "Patriots",
-        id: "620701254666027008"
-    },
-    No: {
-        name: "Saints",
-        id: "620701259913232390"
-    },
-    Nyg: {
-        name: "Giants",
-        id: "620701250811330560"
-    },
-    Nyj: {
-        name: "Jets",
-        id: "620701258390437888"
-    },
-    Lv: {
-        name: "Raiders",
-        id: "620701260315754566"
-    },
-    Phi: {
-        name: "Eagles",
-        id: "620701261058015245"
-    },
-    Pit: {
-        name: "Steelers",
-        id: "620701260072353809"
-    },
-    Sf: {
-        name: "49ers",
-        id: "620701258814324746"
-    },
-    Sea: {
-        name: "Seahawks",
-        id: "620701255605420042"
-    },
-    Tb: {
-        name: "Buccanners",
-        id: "620701261720715284"
-    },
-    Ten: {
-        name: "Titans",
-        id: "620701260970196995"
-    },
-    Was: {
-        name: "Football_Team",
-        id: "741681801336193045"
-    },
-
-}
+  Ari: {
+    name: "Cardinals",
+    emojiName: "Arizona_Cardinals_logo"
+  },
+  Atl: {
+    name: "Falcons",
+    emojiName: "Atlanta_Falcons_logo"
+  },
+  Bal: {
+    name: "Ravens",
+    emojiName: "Baltimore_Ravens_logo"
+  },
+  Buf: {
+    name: "Bills",
+    emojiName: "Buffalo_Bills_logo"
+  },
+  Car: {
+    name: "Panthers",
+    emojiName: "Carolina_Panthers_logo"
+  },
+  Chi: {
+    name: "Bears",
+    emojiName: "Chicago_Bears_logo"
+  },
+  Cin: {
+    name: "Bengals",
+    emojiName: "Cincinnati_Bengals_logo"
+  },
+  Cle: {
+    name: "Browns",
+    emojiName: "Cleveland_Browns_logo"
+  },
+  Dal: {
+    name: "Cowboys",
+    emojiName: "Dallas_Cowboys_logo"
+  },
+  Den: {
+    name: "Broncos",
+    emojiName: "Denver_Broncos_logo"
+  },
+  Det: {
+    name: "Lions",
+    emojiName: "Detroit_Lions_logo"
+  },
+  Gb: {
+    name: "Packers",
+    emojiName: "Green_Bay_Packers_logo"
+  },
+  Hou: {
+    name: "Texans",
+    emojiName: "Houston_Texans_logo"
+  },
+  Ind: {
+    name: "Colts",
+    emojiName: "Indianapolis_Colts_logo"
+  },
+  Jax: {
+    name: "Jaguars",
+    emojiName: "Jacksonville_Jaguars_logo"
+  },
+  Kc: {
+    name: "Chiefs",
+    emojiName: "Kansas_City_Chiefs_logo"
+  },
+  Lac: {
+    name: "Chargers",
+    emojiName: "Los_Angeles_Chargers_logo"
+  },
+  Lar: {
+    name: "Rams",
+    emojiName: "Los_Angeles_Rams_logo"
+  },
+  Mia: {
+    name: "Dolphins",
+    emojiName: "Miami_Dolphins_logo"
+  },
+  Min: {
+    name: "Vikings",
+    emojiName: "Minnesota_Vikings_logo"
+  },
+  Ne: {
+    name: "Patriots",
+    emojiName: "New_England_Patriots_logo"
+  },
+  No: {
+    name: "Saints",
+    emojiName: "New_Orleans_Saints_logo"
+  },
+  Nyg: {
+    name: "Giants",
+    emojiName: "New_York_Giants_logo"
+  },
+  Nyj: {
+    name: "Jets",
+    emojiName: "New_York_Jets_logo"
+  },
+  Lv: {
+    name: "Raiders",
+    emojiName: "Las_Vegas_Raiders_logo"
+  },
+  Phi: {
+    name: "Eagles",
+    emojiName: "Philadelphia_Eagles_logo"
+  },
+  Pit: {
+    name: "Steelers",
+    emojiName: "Pittsburgh_Steelers_logo"
+  },
+  Sf: {
+    name: "49ers",
+    emojiName: "San_Francisco_49ers_logo"
+  },
+  Sea: {
+    name: "Seahawks",
+    emojiName: "Seattle_Seahawks_logo"
+  },
+  Tb: {
+    name: "Buccaneers",
+    emojiName: "Tampa_Bay_Buccaneers_logo"
+  },
+  Ten: {
+    name: "Titans",
+    emojiName: "Tennessee_Titans_logo"
+  },
+  Was: {
+    name: "Commanders",
+    emojiName: "Washington_Commanders_logo"
+  }
+};
 
 exports.nfl = nfl;
