16dd3381a792726f25263104f65724622d64d1a2
tugboat0278 <tugboat0278@gmail.com>
2026-09-05 03:17:19 -0400

    Delete commands/misc/stupidBot.js

--- commands/misc/stupidBot.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    commands: ['stupidbot', 'StupidBot', 'stupidBot', 'Stupidbot'],
-    minArgs: 0,
-    maxArgs: 0,
-    callback: (message, arguments, text) => {
-        message.reply(`That's not very nice.`);
-    }
-}
