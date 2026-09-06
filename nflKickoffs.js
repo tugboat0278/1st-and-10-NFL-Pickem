61b132b306f4a96f42f4f12e488ebb218207aaa7
tugboat0278 <tugboat0278@gmail.com>
2026-09-05 22:37:44 -0400

    Update nflKickoffs.js

--- nflKickoffs.js
+++ nflKickoffs.js
@@ -38,5 +38,61 @@ module.exports = {
 
     // Monday Night Football
     { game: 16, kickoff: '2026-09-14T20:15:00-04:00' }
+  ],
+
+  2: [
+    // Thursday, September 17
+    // DET @ BUF
+    { game: 1, kickoff: '2026-09-17T20:15:00-04:00' },
+
+    // Sunday, September 20 — 1:00 PM ET
+    // MIN @ CHI
+    { game: 2, kickoff: '2026-09-20T13:00:00-04:00' },
+
+    // PHI @ TEN
+    { game: 3, kickoff: '2026-09-20T13:00:00-04:00' },
+
+    // GB @ NYJ
+    { game: 4, kickoff: '2026-09-20T13:00:00-04:00' },
+
+    // CAR @ ATL
+    { game: 5, kickoff: '2026-09-20T13:00:00-04:00' },
+
+    // NO @ BAL
+    { game: 6, kickoff: '2026-09-20T13:00:00-04:00' },
+
+    // CIN @ HOU
+    { game: 7, kickoff: '2026-09-20T13:00:00-04:00' },
+
+    // CLE @ TB
+    { game: 8, kickoff: '2026-09-20T13:00:00-04:00' },
+
+    // PIT @ NE
+    { game: 9, kickoff: '2026-09-20T13:00:00-04:00' },
+
+    // Sunday, September 20 — 4:05 PM ET
+    // LV @ LAC
+    { game: 10, kickoff: '2026-09-20T16:05:00-04:00' },
+
+    // JAX @ DEN
+    { game: 11, kickoff: '2026-09-20T16:05:00-04:00' },
+
+    // Sunday, September 20 — 4:25 PM ET
+    // WAS @ DAL
+    { game: 12, kickoff: '2026-09-20T16:25:00-04:00' },
+
+    // SEA @ ARI
+    { game: 13, kickoff: '2026-09-20T16:25:00-04:00' },
+
+    // MIA @ SF
+    { game: 14, kickoff: '2026-09-20T16:25:00-04:00' },
+
+    // Sunday Night Football
+    // IND @ KC
+    { game: 15, kickoff: '2026-09-20T20:20:00-04:00' },
+
+    // Monday Night Football
+    // NYG @ LAR
+    { game: 16, kickoff: '2026-09-21T20:15:00-04:00' }
   ]
 };
