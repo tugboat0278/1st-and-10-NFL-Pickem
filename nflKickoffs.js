f36826cb3757ad70b152cf1760d9b75b2ed2489b
tugboat0278 <tugboat0278@gmail.com>
2026-09-05 20:25:29 -0400

    Update nflKickoffs.js

--- nflKickoffs.js
+++ nflKickoffs.js
@@ -1,1 +1,32 @@
+// 2026 NFL regular-season kickoff times.
+//
+// IMPORTANT:
+// These are Eastern Time (ET) kickoff times.
+// The bot uses this information to automatically lock each individual
+// matchup once that real NFL game begins.
+//
+// Format:
+// week: [
+//   { game: 1, kickoff: 'YYYY-MM-DDTHH:mm:ss-04:00' }
+// ]
 
+module.exports = {
+  1: [
+    { game: 1, kickoff: '2026-09-10T20:20:00-04:00' },
+    { game: 2, kickoff: '2026-09-13T16:25:00-04:00' },
+    { game: 3, kickoff: '2026-09-13T13:00:00-04:00' },
+    { game: 4, kickoff: '2026-09-13T13:00:00-04:00' },
+    { game: 5, kickoff: '2026-09-13T13:00:00-04:00' },
+    { game: 6, kickoff: '2026-09-13T13:00:00-04:00' },
+    { game: 7, kickoff: '2026-09-13T13:00:00-04:00' },
+    { game: 8, kickoff: '2026-09-13T13:00:00-04:00' },
+    { game: 9, kickoff: '2026-09-13T13:00:00-04:00' },
+    { game: 10, kickoff: '2026-09-13T13:00:00-04:00' },
+    { game: 11, kickoff: '2026-09-13T16:05:00-04:00' },
+    { game: 12, kickoff: '2026-09-13T16:05:00-04:00' },
+    { game: 13, kickoff: '2026-09-13T16:25:00-04:00' },
+    { game: 14, kickoff: '2026-09-13T20:20:00-04:00' },
+    { game: 15, kickoff: '2026-09-14T19:00:00-04:00' },
+    { game: 16, kickoff: '2026-09-14T22:15:00-04:00' }
+  ]
+};
